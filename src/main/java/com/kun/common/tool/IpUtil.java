75a60f8e125709bb2e6a3e47469804f720a9e226
huangkun <kunh@everstring.com>
2015-08-28 11:52:12 +0800

    extract the real ip of client from httpServletRequest

---
+++ src/main/java/com/kun/common/tool/IpUtil.java
@@ -0,0 +1,25 @@
+package com.kun.common.tool;
+import javax.servlet.http.HttpServletRequest;
+
+
+
+public class IpUtil {
+    /**
+     * extract the real ip of client from request
+     * @param request reques object
+     * @return real client ip 
+     */
+    public static String getIpAddr(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknow".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
+    }
+}
